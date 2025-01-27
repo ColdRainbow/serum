08c9a7ca43bbe99c2f662ae611597c3a4a150f79
Arti <arti@lido.fi>
2025-01-27 15:33:59 +0100

    Revert "Add an option to use root derivation path"
    This reverts commit 7d02d2be1acf923925a30aa2ed9d27be309629fc.

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,376 @@
-use std::rc::Rc;
-
-use anchor_client::Cluster;
-use base64::{prelude::BASE64_STANDARD, Engine};
-use clap::Parser;
+use anchor_client::{
+    anchor_lang::{prelude::AccountMeta, solana_program::hash},
+    solana_sdk::{
+        commitment_config::CommitmentConfig,
+        instruction::Instruction,
+        message::Message,
+        pubkey::Pubkey as AnchorPubkey,
+        signature::{Keypair, Signature, Signer},
+        system_instruction, sysvar,
+        transaction::Transaction,
+    },
+    Client, Cluster,
+};
+use base64::{engine::Engine, prelude::BASE64_STANDARD};
+use clap::{Args, Parser, Subcommand};
+use coral_multisig::instruction as multisig_instructions;
+use coral_multisig::{accounts as multisig_accounts, TransactionAccount};
 use crossterm::style::{style, Stylize};
-use solana_remote_wallet::{
-    ledger::LedgerWallet,
-    locator::Manufacturer,
-    remote_wallet::{RemoteWallet, RemoteWalletError},
-};
-use solana_sdk::{
-    derivation_path::DerivationPath,
-    pubkey::Pubkey,
-    signature::Keypair,
-    signer::{EncodableKey, Signer, SignerError},
-};
+use spl_token::instruction::{self as token_instruction, TokenInstruction};
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(long = "ledger", default_value_t = false)]
-    ledger: bool,
-    #[arg(short = 'n', long = "account-number")]
-    account_number: Option<u32>,
-
+    #[arg(
+        long = "pid",
+        default_value = "AAHT26ecV3FEeFmL2gDZW6FfEqjPkghHbAkNZGqwT8Ww" // Devnet: msigUdDBsR4zSUYqYEDrc1LcgtmuSDDM7KxpRUXNC6U
+    )]
+    pid: AnchorPubkey,
     #[arg(long = "cluster", default_value_t = Cluster::Devnet)]
     cluster: Cluster,
 
     #[arg(short = 'k', long = "private-key")]
     key_file: Option<String>,
 
-    transaction: String,
+    #[command(subcommand)]
+    command: Command,
 }
 
-#[derive(Clone)]
-struct LedgerSigner {
-    ledger: Rc<LedgerWallet>,
-    derivation_path: DerivationPath,
+#[derive(Args)]
+struct SignerArg {
+    #[arg(long = "signer")]
+    signer: AnchorPubkey,
+    #[arg(long = "nonce-account")]
+    nonce_account: AnchorPubkey,
+    #[arg(long = "nonce")]
+    nonce: hash::Hash,
 }
 
-impl Signer for LedgerSigner {
-    fn try_pubkey(&self) -> Result<Pubkey, solana_sdk::signer::SignerError> {
-        Ok(self
-            .ledger
-            .get_pubkey(&self.derivation_path, false)
-            .map_err(ledger_to_signer_error)?
-            .to_bytes()
-            .into())
-    }
-
-    fn try_sign_message(
-        &self,
-        message: &[u8],
-    ) -> Result<solana_sdk::signature::Signature, solana_sdk::signer::SignerError> {
-        Ok(self
-            .ledger
-            .sign_message(&self.derivation_path, message)
-            .map_err(ledger_to_signer_error)?
-            .as_ref()
-            .try_into()
-            .unwrap())
-    }
-
-    fn is_interactive(&self) -> bool {
-        true
-    }
+#[derive(Subcommand)]
+enum Command {
+    /// Create new multisig
+    #[command(hide(true))]
+    CreateMultisig {
+        #[command(flatten)]
+        signer: SignerArg,
+        #[arg(long = "signers")]
+        signers: Vec<AnchorPubkey>,
+        #[arg(long = "threshold")]
+        threshold: u64,
+    },
+    /// Create a token transfer transaction
+    CreateTokenTransferTransaction {
+        #[command(flatten)]
+        signer: SignerArg,
+        #[arg(long = "multisig")]
+        multisig: AnchorPubkey,
+        #[arg(long = "from")]
+        from: AnchorPubkey,
+        #[arg(long = "to")]
+        to: AnchorPubkey,
+        #[arg(long = "amount")]
+        amount: f64,
+    },
+    /// Approve a pending transaction
+    Approve {
+        #[command(flatten)]
+        signer: SignerArg,
+        #[arg(long = "multisig")]
+        multisig: AnchorPubkey,
+        #[arg(long = "transaction")]
+        transaction: AnchorPubkey,
+    },
+    /// Execute an approved transaction
+    ExecuteTokenTransferTransaction {
+        #[command(flatten)]
+        signer: SignerArg,
+        #[arg(long = "multisig")]
+        multisig: AnchorPubkey,
+        #[arg(long = "transaction")]
+        transaction: AnchorPubkey,
+    },
+    /// Submit a signed transaction
+    Submit {
+        #[arg(long = "transaction")]
+        transaction: String,
+        #[arg(long = "signatures")]
+        signatures: Vec<Signature>,
+    },
 }
 
-fn ledger_to_signer_error(e: RemoteWalletError) -> SignerError {
-    SignerError::Custom(e.to_string())
+fn build_tx(
+    payer: AnchorPubkey,
+    nonce: hash::Hash,
+    nonce_authority: AnchorPubkey,
+    instructions: Vec<Instruction>,
+) -> anyhow::Result<Message> {
+    let mut message = Message::new_with_nonce(instructions, Some(&payer), &nonce_authority, &payer);
+    message.recent_blockhash = nonce;
+    println!("You may now check the transaction using external tools.\nHere is the transaction data in base64:\n\n{}\n",
+        BASE64_STANDARD.encode(message.serialize())
+    );
+
+    Ok(message)
 }
 
-fn run(signer: impl Signer, cli: Cli) -> anyhow::Result<()> {
-    let message = BASE64_STANDARD.decode(cli.transaction)?;
-    let sig = signer.sign_message(&message);
-    println!("Message signed: {}", style(sig).green());
-    Ok(())
-}
-
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
+    let dummy_signer = Keypair::new();
+    let client =
+        Client::new_with_options(cli.cluster, &dummy_signer, CommitmentConfig::processed());
 
-    if cli.ledger {
-        let wallet_manager = solana_remote_wallet::remote_wallet::initialize_wallet_manager()?;
-        wallet_manager.update_devices()?;
-        let ledger_info = wallet_manager
-            .list_devices()
-            .into_iter()
-            .find(|wi| matches!(wi.manufacturer, Manufacturer::Ledger))
-            .ok_or(anyhow::Error::msg("Ledger not found. Please, ensure that it is connected, unlocked, and the Solana app is opened"))?;
-        let ledger = wallet_manager.get_ledger(&ledger_info.host_device_path)?;
-        let signer = LedgerSigner {
-            ledger,
-            derivation_path: DerivationPath::new_bip44(cli.account_number, None),
-        };
-        run(signer, cli)?;
-    } else {
-        run(
-            Rc::new(
-                Keypair::read_from_file(
-                    cli.key_file
-                        .clone()
-                        .ok_or(anyhow::Error::msg("private-key is required"))?,
-                )
-                .unwrap(),
-            ),
-            cli,
-        )?;
-    };
+    // Program instance
+    let program = client.program(cli.pid)?;
+
+    match cli.command {
+        Command::CreateMultisig {
+            signer,
+            signers,
+            threshold,
+        } => {
+            let keypair = Keypair::new();
+            let accounts = multisig_accounts::CreateMultisig {
+                multisig: keypair.pubkey(),
+            };
+            let (multisig_pda, nonce) = derive_multisig_signer(&keypair.pubkey(), &cli.pid);
+            let instructions = multisig_instructions::CreateMultisig {
+                owners: signers,
+                threshold,
+                nonce,
+            };
+            let req = program
+                .request()
+                .accounts(accounts)
+                .accounts(AccountMeta::new_readonly(sysvar::rent::id(), false))
+                .args(instructions)
+                .instruction(system_instruction::create_account(
+                    &signer.signer,
+                    &keypair.pubkey(),
+                    program.rpc().get_minimum_balance_for_rent_exemption(500)?,
+                    500,
+                    &program.id(),
+                ));
+
+            let tx = build_tx(
+                signer.signer,
+                signer.nonce,
+                signer.nonce_account,
+                req.instructions()?,
+            )?;
+            let sig = keypair.sign_message(&tx.serialize());
+
+            println!("Transaction signed by multisig account: {}", sig);
+            println!("Multisig address: {}", keypair.pubkey());
+            println!("Multisig PDA: {}", multisig_pda);
+        }
+        Command::CreateTokenTransferTransaction {
+            signer,
+            multisig,
+            from,
+            to,
+            amount,
+        } => {
+            println!(
+                "{}",
+                "Preparing a token transfer transaction with the following parameters:".bold()
+            );
+            println!(
+                "Multisig address: {}\nFrom address: {}\nTo address: {}\nAmount: {}\n",
+                style(multisig).green(),
+                style(from).green(),
+                style(to).green(),
+                style(amount).green(),
+            );
+
+            let from_account = program
+                .async_rpc()
+                .get_token_account(&from)
+                .await?
+                .ok_or(anyhow::Error::msg("source token account not found"))?;
+            let to_account = program
+                .async_rpc()
+                .get_token_account(&to)
+                .await?
+                .ok_or(anyhow::Error::msg("destination token account not found"))?;
+            if from_account.mint != to_account.mint {
+                return Err(anyhow::Error::msg(
+                    "source and destination accounts have different mint addresses",
+                ));
+            }
+            if from_account.token_amount.ui_amount.is_none()
+                || from_account.token_amount.ui_amount.unwrap() < amount
+            {
+                return Err(anyhow::Error::msg(
+                    "source account doesn't have sufficient amount of token",
+                ));
+            }
+
+            let amount = spl_token::ui_amount_to_amount(amount, from_account.token_amount.decimals);
+
+            let keypair = Keypair::new();
+            let (multisig_pda, _) = derive_multisig_signer(&multisig, &cli.pid);
+            let transfer = token_instruction::transfer(
+                &spl_token::id(),
+                &from.to_bytes().into(),
+                &to.to_bytes().into(),
+                &multisig_pda.to_bytes().into(),
+                &[],
+                amount,
+            )?;
+
+            let transfer_accounts: Vec<TransactionAccount> =
+                transfer.accounts.iter().map(Into::into).collect();
+            let accounts = multisig_accounts::CreateTransaction {
+                multisig,
+                transaction: keypair.pubkey(),
+                proposer: signer.signer,
+            };
+            let instructions = multisig_instructions::CreateTransaction {
+                pid: spl_token::id().to_bytes().into(),
+                accs: transfer_accounts,
+                data: transfer.data,
+            };
+            let req = program
+                .request()
+                .accounts(accounts)
+                .accounts(AccountMeta::new_readonly(sysvar::rent::id(), false))
+                .args(instructions)
+                .instruction(system_instruction::create_account(
+                    &signer.signer,
+                    &keypair.pubkey(),
+                    program.rpc().get_minimum_balance_for_rent_exemption(500)?,
+                    500,
+                    &program.id(),
+                ));
+
+            let tx = build_tx(
+                signer.signer,
+                signer.nonce,
+                signer.nonce_account,
+                req.instructions()?,
+            )?;
+            let sig = keypair.sign_message(&tx.serialize());
+
+            println!(
+                "Transaction signed by transaction account: {}",
+                style(sig).green()
+            );
+            println!(
+                "Pending transaction account: {}",
+                style(keypair.pubkey()).green()
+            );
+        }
+        Command::Approve {
+            signer,
+            multisig,
+            transaction,
+        } => {
+            println!(
+                "{}",
+                "Approving a transaction with the following parameters:".bold()
+            );
+            println!(
+                "Multisig address: {}\nTransaction address: {}\n",
+                style(multisig).green(),
+                style(transaction).green(),
+            );
+
+            let accounts = multisig_accounts::Approve {
+                multisig,
+                transaction,
+                owner: signer.signer,
+            };
+            let instructions = multisig_instructions::Approve {};
+            let req = program.request().accounts(accounts).args(instructions);
+
+            build_tx(
+                signer.signer,
+                signer.nonce,
+                signer.nonce_account,
+                req.instructions()?,
+            )?;
+        }
+        Command::ExecuteTokenTransferTransaction {
+            signer,
+            multisig,
+            transaction,
+        } => {
+            let transaction_account: coral_multisig::Transaction =
+                program.account(transaction).await?;
+            let (multisig_pda, _) = derive_multisig_signer(&multisig, &cli.pid);
+            let mut remaining_accounts: Vec<AccountMeta> = transaction_account
+                .accounts
+                .iter()
+                .map(Into::into)
+                .collect();
+            for acc in remaining_accounts.iter_mut() {
+                acc.is_signer = false;
+            }
+            let from_account = program
+                .async_rpc()
+                .get_token_account(&remaining_accounts[0].pubkey)
+                .await?
+                .ok_or(anyhow::Error::msg("source token account not found"))?;
+            let amount = match TokenInstruction::unpack(&transaction_account.data)? {
+                TokenInstruction::Transfer { amount } => Ok(amount),
+                _ => Err(anyhow::Error::msg(
+                    "transaction instruction is not transfer",
+                )),
+            }?;
+            let amount = spl_token::amount_to_ui_amount(amount, from_account.token_amount.decimals);
+            println!("Executing a token transfer transaction with the following parameters:");
+            println!(
+                "Multisig address: {}\nTransaction address: {}\nFrom: {}\nTo: {}\nAmount: {}\n",
+                style(multisig).green(),
+                style(transaction).green(),
+                style(remaining_accounts[0].pubkey).green(),
+                style(remaining_accounts[1].pubkey).green(),
+                style(amount).green(),
+            );
+
+            let accounts = multisig_accounts::ExecuteTransaction {
+                multisig,
+                multisig_signer: multisig_pda,
+                transaction,
+            };
+            let instructions = multisig_instructions::ExecuteTransaction {};
+            let req = program
+                .request()
+                .accounts(accounts)
+                .accounts(remaining_accounts)
+                .accounts(AccountMeta::new(spl_token::id().to_bytes().into(), false))
+                .args(instructions);
+
+            build_tx(
+                signer.signer,
+                signer.nonce,
+                signer.nonce_account,
+                req.instructions()?,
+            )?;
+        }
+        Command::Submit {
+            transaction,
+            signatures,
+        } => {
+            let data = BASE64_STANDARD.decode(transaction)?;
+            let message: Message = bincode::deserialize(&data)?;
+            let tx = Transaction {
+                signatures,
+                message,
+            };
+            let sig = program
+                .async_rpc()
+                .send_and_confirm_transaction(&tx)
+                .await?;
+            println!("Transaction submitted: {}", style(sig).green());
+        }
+    }
     Ok(())
+}
+
+fn derive_multisig_signer(
+    multisig: &AnchorPubkey,
+    program_id: &AnchorPubkey,
+) -> (AnchorPubkey, u8) {
+    AnchorPubkey::find_program_address(&[multisig.as_ref()], program_id)
 }
